49b1d1f71cefd0db37ed7f9552ad153a72cfde4b
Katsiaryna Zhalabkevich <beluzhka@gmail.com>
2020-10-03 23:53:35 +0300

    docs: add index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,23 @@
 
-exports.min = function min (array) {
-  return 0;
+const isArray = (array) => array && array.length;
+
+exports.min = (array) => {
+    if (isArray(array)) {
+        return Math.min(...array);
+    }
+    return 0;
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = (array) => {
+    if (isArray(array)) {
+        return Math.max(...array);
+    }
+    return 0;
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = (array) => {
+    if (isArray(array)) {
+        return array.reduce( (acc, currVal) => acc + currVal) / array.length;
+    }
+    return 0;
 }
